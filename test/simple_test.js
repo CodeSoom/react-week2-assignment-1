75794ab9e491b3276548f903fb9d379878f284d5
hannut91 <hannut91@gmail.com>
2020-06-05 16:59:53 +0900

    First commit

---
+++ test/simple_test.js
@@ -0,0 +1,63 @@
+Feature('Click me');
+
+const range = (count) => [...Array(count)];
+
+Scenario('Click me test', (I) => {
+  I.amOnPage('/');
+
+  I.see('(0)');
+
+  range(5).forEach(() => I.click('Click me'));
+
+  I.see('(5)');
+});
+
+Scenario('Click 1 test', (I) => {
+  I.amOnPage('/');
+
+  I.see('(0)');
+
+  range(5).forEach(() => I.click('1'));
+
+  I.see('(5)');
+});
+
+Scenario('Click 2 test', (I) => {
+  I.amOnPage('/');
+
+  I.see('(0)');
+
+  range(5).forEach(() => I.click('2'));
+
+  I.see('(10)');
+});
+
+Scenario('Click 3 test', (I) => {
+  I.amOnPage('/');
+
+  I.see('(0)');
+
+  range(5).forEach(() => I.click('3'));
+
+  I.see('(15)');
+});
+
+Scenario('Click 1 test', (I) => {
+  I.amOnPage('/');
+
+  I.see('(0)');
+
+  range(5).forEach(() => I.click('4'));
+
+  I.see('(20)');
+});
+
+Scenario('Click 1 test', (I) => {
+  I.amOnPage('/');
+
+  I.see('(0)');
+
+  range(5).forEach(() => I.click('5'));
+
+  I.see('(25)');
+});
