6eeff93a9004810dcad4acf5f7c8e6a0e14ad8ca
eonsang <2eonhworking@gmail.com>
2020-06-08 09:09:15 +0900

    각각의 element 작업 완료

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom';
 
 function App() {
   return (
     <div>
-      <p>hello</p>
+      <h1>Counter</h1>
+      <button type="button">Click me!! (0)</button>
+      <br />
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button">4</button>
+      <button type="button">5</button>
     </div>
   );
 }
