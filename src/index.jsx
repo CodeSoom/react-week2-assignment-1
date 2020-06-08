5d6a8c12d28d8f156dfe0e065bda663f4391e2b3
eonsang <2eonhworking@gmail.com>
2020-06-08 09:06:43 +0900

    reactdom.render()를 활용하여 createElement 작업  완료

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <p>hello</p>
+    </div>
+  );
+}
+
+ReactDom.render(<App />, document.getElementById('app'));
