1673907ed75e91946dce38c6604e01a9bc533274
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-08 14:27:59 +0900

    feat(index.jsx): draw title
    - Counter가 웹페이지에서 보이도록 구현

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div>
+            <p>Counter</p>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
