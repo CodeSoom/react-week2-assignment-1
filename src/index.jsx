153240fc9ee8fe2a81d388c9664b039eff132b9e
eonsang <2eonhworking@gmail.com>
2020-06-08 09:12:09 +0900

    count state 붙임

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,18 @@ import React, { useState } from 'react';
 import ReactDom from 'react-dom';
 
 function App() {
+  const [state, setState] = useState({
+    count: 0,
+  });
+  const { count } = state;
   return (
     <div>
       <h1>Counter</h1>
-      <button type="button">Click me!! (0)</button>
+      <button type="button">
+        Click me!! (
+        {count}
+        )
+      </button>
       <br />
       <button type="button">1</button>
       <button type="button">2</button>
