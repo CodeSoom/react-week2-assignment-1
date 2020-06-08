eb7d2b18db8984c6e05793a0486e98833c7ac127
yeonhwa <yeonhwa6717@gmail.com>
2020-06-08 22:43:17 +0900

    count초기화
    `useState`로 상태 관리

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function Button({ children }) {
@@ -8,10 +8,17 @@ function Button({ children }) {
 }
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <p>Conter</p>
-      <p><Button>Click me!(0)</Button></p>
+      <p>
+        <Button>
+          Click me!(
+          { count }
+          )
+        </Button>
+      </p>
       <p>
         {[1, 2, 3, 4, 5].map((number) => (
           <Button key={number}>{number}</Button>
