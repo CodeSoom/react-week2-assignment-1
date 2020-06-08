3208af623035974131952092e5d05c2da65c9140
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-08 14:57:28 +0900

    feat(index.jsx): number buttons
    - 1,2,3,4,5 버튼을 웹페이지로 출력하는 기능 구현

--- src/index.jsx
+++ src/index.jsx
@@ -12,13 +12,15 @@ function App() {
   function handleCounterClick() {
     setState({
       count: count + 1,
-    })
+    });
   };
 
   return (
     <div>
       <p>Counter</p>
       <CounterButton count={count} onClick={handleCounterClick}/>
+      <br />
+      {[1,2,3,4,5].map(num => <button type="button">{num}</button>)}
     </div>
   );
 }
