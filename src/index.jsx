8d4901885d9ff9387c55b1e2665f5cda9101460b
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-08 14:29:11 +0900

    style(index.jsx): change indent
    - ESLint를 지키기 위해 indent를 4에서 2로 변경

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-    return (
-        <div>
-            <p>Counter</p>
-        </div>
-    );
+  return (
+    <div>
+      <p>Counter</p>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
