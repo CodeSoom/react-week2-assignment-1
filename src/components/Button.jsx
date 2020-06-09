64a967f77a569b37f5d53441eea012a12685070e
Law Woo <law@cupist.com>
2020-06-10 00:43:39 +0900

    Update codes to prevent no-number click action

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
 function Button({ children, onClick }) {
+  if (typeof children === 'number') {
+    return (
+      <button type="button" onClick={onClick}>
+        {children}
+      </button>
+    );
+  }
+
   return (
-    <button type="button" onClick={onClick}>
+    <button type="button">
       {children}
     </button>
   );
