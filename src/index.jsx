2fe88467559183835331961333b7c7e9ba9893ce
sogoagain <imyong0@gmail.com>
2020-06-09 23:34:03 +0900

    버튼을 누르면 숫자가 증가한다.
    - Click me! 버튼을 누르면 숫자가 1씩 증가한다.
    - 숫자 버튼을 누르면 버튼에 적힌 숫자만큼 증가한다.
    - count 변수를 다루는 핸들러는 공통으로 사용한다.

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
-  <>
-    <p>Counter</p>
-    <p>
-      <button type="button">Click me! (0)</button>
-    </p>
-    <p>
-      {[1, 2, 3, 4, 5].map((i) => (
-        <button type="button" key={i}>
-          {i}
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleCounter = (number) => {
+    setCount(number);
+  };
+
+  return (
+    <>
+      <p>Counter</p>
+      <p>
+        <button type="button" onClick={() => handleCounter(count + 1)}>
+          Click me! (
+          {count}
+          )
         </button>
-      ))}
-    </p>
-  </>
-);
+      </p>
+      <p>
+        {[1, 2, 3, 4, 5].map((i) => (
+          <button type="button" key={i} onClick={() => handleCounter(count + i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
