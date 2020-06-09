7c6c95a95054456684e0f741376f7b129e1996d1
Law Woo <law@cupist.com>
2020-06-09 09:37:41 +0900

    Update clearly codes about number input

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -5,7 +5,7 @@ function Buttons({ onClick }) {
   return (
     <p>
       {[1, 2, 3, 4, 5].map((i) => (
-        <Button key={i} onClick={onClick}>
+        <Button key={i} onClick={() => onClick(i)}>
           {i}
         </Button>
       ))}
