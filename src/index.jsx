983ae28bbea01b227de4aa6d1c5a07b3aed706ba
aumilkyeong <aumilkyeong@gmail.com>
2020-06-10 16:53:21 +0900

    기능 완성 및 린트, 인수테스트 통과

--- src/index.jsx
+++ src/index.jsx
@@ -21,16 +21,43 @@ function MainButton({ count, onClick }) {
   );
 }
 
+function ButtonGroup({ count, onClick }) {
+  const numbers = [1, 2, 3, 4, 5];
+  return (
+    <p>
+      {numbers.map((number) => (
+        <NumberButton
+          count={count}
+          number={number}
+          onClick={onClick}
+        />
+      ))}
+    </p>
+  );
+}
+
+function NumberButton({ count, number, onClick }) {
+  return (
+    <button type="button" onClick={() => onClick({ count, number })}>
+      {number}
+    </button>
+  );
+}
+
 function App() {
   const [state, setState] = useState({
     count: 0,
-    numbers: [1, 2, 3, 4, 5],
-    increment: 0,
   });
 
   function handleMainClick({ count }) {
     setState({
       count: count + 1,
+    });
+  }
+
+  function handleNumberClick({ count, number }) {
+    setState({
+      count: count + number,
     });
   }
 
@@ -42,6 +69,10 @@ function App() {
       <MainButton
         count={count}
         onClick={handleMainClick}
+      />
+      <ButtonGroup
+        count={count}
+        onClick={handleNumberClick}
       />
     </div>
   );
