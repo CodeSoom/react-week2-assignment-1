c66cb64c9a8882de484fe37056aabf6eb7dcf0b3
newoo <newoo4297@naver.com>
2020-06-10 09:40:25 +0900

    Delete conditional statement to check if number or not

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 
 function Button({ children, onClick }) {
-  if (typeof children === 'number') {
-    return (
-      <button type="button" onClick={onClick}>
-        {children}
-      </button>
-    );
-  }
-
   return (
-    <button type="button">
+    <button type="button" onClick={onClick}>
       {children}
     </button>
   );
