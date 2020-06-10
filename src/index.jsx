49468d07aabd6c6f7cb1ec6d49cfe9eeda933181
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-10 23:39:03 +0900

    Update counter app with react components
    Submit assignment2-1 as the fourth try

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Page from './Page';
-
-function App() {
-  const [state, setState] = useState({
-    count: 0,
-  });
-
-  const { count } = state;
-
-  function handleClick(i) {
-    setState({
-      count: count + i,
-    });
-  }
-  return (
-    <Page
-      count={count}
-      onClick={handleClick}
-    />
-  );
-}
-
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
