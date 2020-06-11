e13f41faa3095a18bdf5e5e170ca1965586d0d5b
Joylish <yeonzzu2@gmail.com>
2020-06-11 21:14:43 +0900

    apply for separation of intersts

--- src/index.jsx
+++ src/index.jsx
@@ -5,29 +5,15 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-let count = 0;
 
-function Counter(){
-    ////////////////////////////////
-    //useState를 통해서 Counter 컴포넌트에 갇힌 state 생성
-    //초기에 state에 object가 할당
-    //이후 setState를 통해서 object 내부 값 변경
-    const [state, setState] = useState({
-        count: 0,
-    })
-
-    const {count} = state;
-
-    function handleClick() {
-        console.log('click');
-        setState({
-            count: count + 1,
-        })
-    }
-    ////////////////////////////////
+//Counter 컴포넌트; count의 값을 보여주는 역할(ui)
+function Counter({count, onClick}){
     return(
-        <button type = "button" onClick={handleClick}>
-            Click me! ({count})
+        <button type = "button" onClick={onClick}>
+            Click me! 
+            (
+            {count}
+            )
         </button>
     )
 }
@@ -49,20 +35,43 @@ function Buttons() {
         </Button>
     ));
 }
-function Apps() {
+
+//count가 어떻게 처리되는지는 모른다.
+function Page({count, onClick}){
     return (
         <div>
             <p>hello world</p>
-            <Counter />
+            <p><Counter
+                count={count}
+                onClick={onClick} />
+            </p>
             <Buttons />
         </div>
     );
 }
 
-function render(){
-    ReactDOM.render(
-    <Apps />,
-    document.getElementById('app')
+//App 컴포넌트; 보여지는 건 관심 없고, 상태 관리(business logic)
+function App() {
+    const [state, setState] = useState({
+        count: 0,
+    });
+
+    const { count } =state;
+
+    function handleClick(){
+        setState({
+            count: count + 1,
+        })
+    }
+
+    return (
+        <Page
+        count = {count}
+        onClick = {handleClick}
+        />
     );
 }
-render()
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
