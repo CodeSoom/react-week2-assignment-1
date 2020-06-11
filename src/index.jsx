af78ae7e60d03f3bfb5d7686c9d045b9ca10d96a
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-06-10 23:00:40 -0700

    add: counter

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,27 @@
 /* @jsx React.createElement */
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+
+function Counter() {
+  const [state, setState] = useState({
+    count: 0,
+  });
+  const { count } = state;
+
+  function handleCount() {
+    setState({
+      count: count + 1,
+    });
+  }
+
+  return (
+    <button type="button" onClick={handleCount}>
+      counter:
+      {count}
+    </button>
+  );
+}
 
 function Button({ children }) {
   return (
@@ -25,9 +45,10 @@ function Buttons() {
   );
 }
 
-function App() { 
-    return (
+function App() {
+  return (
     <div>
+      <Counter />
       <Buttons />
     </div>
   );
