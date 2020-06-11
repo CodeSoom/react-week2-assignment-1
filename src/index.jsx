a20f9f9c31dc6ac756786cf704fb8ccbc6790870
amorfati0310 <lyhy0310@gmail.com>
2020-06-11 23:42:02 +0900

    수업내용 팔로 컴포넌트 & Props

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+const Buttons = () => (
+    <div>
+        {[1, 2, 3].map((i) => (<Button key={i}>{i}</Button>))}
+    </div>
+);
 
-const element = <h1>Hello, world</h1>;
-render(element, document.getElementById('app'));
+const Button = ({ children }) => (<button>{children}</button>);
+
+
+const App = () => (
+    <div>
+        <p>Hello World !</p>
+        <p>Hi</p>
+        <Buttons />
+    </div>
+);
+render(<App />, document.getElementById('app'));
