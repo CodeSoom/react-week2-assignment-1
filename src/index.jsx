ad0b40a3e9903c637478aac472e6833d8942170b
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-06-10 21:16:53 -0700

    fix: createElement to react

--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,11 @@
-/* @jsx createElement */
-function createElement(tagName, props, ...children) {
-
-    const element = document.createElement(tagName);
-
-    Object.entries(props || {}).forEach(([key, value]) => {
-        element[key.toLowerCase] = value;
-    });
-
-    children.forEach((child) => {
-        if (child instanceof Node) {
-            element.appendChild(child);
-            return;
-        }
-        element.appendChild(document.createTextNode(child));
-    });
-
-    return element;
-
-}
+/* @jsx React.createElement */
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const element = (
-    <div>
-      init
-    </div>
-)
+  <div>
+    lint
+  </div>
+);
 
-document.getElementById('app').textContent('');
-document.getElementById('app').appendChild(element);
+ReactDOM.render(element, document.getElementById('app'));
