8ed32f6278db373d76c29716a483ef1c68de120c
wjdskagjs1 <wjdskagjs1@naver.com>
2020-06-11 17:33:56 +0900

    Delete abbreviations

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -3,8 +3,8 @@ import Button from './Button';
 
 const Buttons = () => {
   const [count, setCount] = useState(0);
-  const handlers = [1, 1, 2, 3, 4, 5].map((i) => () => setCount(count + i));
-  const mapButton = (i) => <Button value={i} onClick={handlers[i]} />;
+  const handlers = [1, 1, 2, 3, 4, 5].map((number) => () => setCount(count + number));
+  const mapButton = (number) => <Button value={number} onClick={handlers[number]} />;
   const components = [<Button value={`Click me! (${count})`} onClick={handlers[0]} />, <p />, ...[1, 2, 3, 4, 5].map(mapButton)];
 
   return (<div>{components.map((element) => element)}</div>);
