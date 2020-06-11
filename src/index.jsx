6789c5e8f397488b292603dfa6caee7c56ca905a
Joylish <yeonzzu2@gmail.com>
2020-06-11 20:53:12 +0900

    change by using react hooks of useState

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,30 @@
 /* @jsx React.createElement */
 
-import React, { useState } from 'react';
 // import React, {children} from 'react'; 하면 왜 안되지?
+// react hooks 중에 useState 사용
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 let count = 0;
 
-function handleClick(){
-    console.log('click');
-    count += 1;
-    console.log(count);
-    render();
-}
-
 function Counter(){
+    ////////////////////////////////
+    //useState를 통해서 Counter 컴포넌트에 갇힌 state 생성
+    //초기에 state에 object가 할당
+    //이후 setState를 통해서 object 내부 값 변경
+    const [state, setState] = useState({
+        count: 0,
+    })
+
+    const {count} = state;
+
+    function handleClick() {
+        console.log('click');
+        setState({
+            count: count + 1,
+        })
+    }
+    ////////////////////////////////
     return(
         <button type = "button" onClick={handleClick}>
             Click me! ({count})
