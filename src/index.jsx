b1fbf0a7c39aaa452b7616438329b4a0a8d07627
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-06-10 21:31:57 -0700

    add: componenets

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+function Button({ value }) {
+  return (
+    <button type="button">
+      {value}
+    </button>
+  );
+}
+
+function Buttons() {
+  return (
+    <p>
+      {
+        [1, 2, 3].map((i) => (
+          <Button key={i}>
+            {i}
+          </Button>
+        ))
+      }
+    </p>
+  );
+}
+
 const element = (
   <div>
-    lint
+    <Buttons />
   </div>
 );
 
