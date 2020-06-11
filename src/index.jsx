3b9d01fc197fe81e38c7544930caa32a60cab394
amorfati0310 <lyhy0310@gmail.com>
2020-06-12 00:08:07 +0900

    수업내용 팔로우 UseState 훅 사용 예제

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 
+
+const Counter = () => {
+  const [state, setState] = useState({
+    count: 0,
+  });
+  const { count } = state;
+  const handleClick = () => {
+    console.log('handleClick');
+    setState({
+      count: count + 1,
+    });
+    console.log(count);
+  };
+
+  return (
+
+    <button type="button" onClick={handleClick}>
+      Click Me (
+      {count}
+      )
+    </button>
+  );
+};
+
 const Buttons = () => (
-    <div>
-        {[1, 2, 3].map((i) => (<Button key={i}>{i}</Button>))}
-    </div>
+  <div>
+    {[1, 2, 3].map((i) => (<Button key={i}>{i}</Button>))}
+  </div>
 );
 
 const Button = ({ children }) => (<button>{children}</button>);
 
 
 const App = () => (
-    <div>
-        <p>Hello World !</p>
-        <p>Hi</p>
-        <Buttons />
-    </div>
+  <div>
+    <p>Hello World !</p>
+    <p>Hi</p>
+    <Buttons />
+    <Counter />
+  </div>
 );
 render(<App />, document.getElementById('app'));
