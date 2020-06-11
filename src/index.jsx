eb60e6ea4bd596311b752ea058e9d7775a085e2a
Joylish <yeonzzu2@gmail.com>
2020-06-11 20:46:20 +0900

    add click me button

--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,57 @@
 /* @jsx React.createElement */
 
-import React from 'react';
+import React, { useState } from 'react';
 // import React, {children} from 'react'; 하면 왜 안되지?
 import ReactDOM from 'react-dom';
 
+let count = 0;
 
+function handleClick(){
+    console.log('click');
+    count += 1;
+    console.log(count);
+    render();
+}
+
+function Counter(){
+    return(
+        <button type = "button" onClick={handleClick}>
+            Click me! ({count})
+        </button>
+    )
+}
 
 function Button({ children }) {
-    return (<button type="button">{children}</button>);
+    return (
+    <button type="button">
+        {children}
+    </button>
+    );
 }
 
 function Buttons() {
     return ([1, 2, 3].map(
         (i) => 
         //선언적 프로그래밍
-        <Button key={i}>{i}</Button>
+        <Button key={i}>
+            {i}
+        </Button>
     ));
 }
 function Apps() {
     return (
         <div>
             <p>hello world</p>
+            <Counter />
             <Buttons />
         </div>
     );
 }
 
-
-ReactDOM.render(<Apps />, document.getElementById('app'));
+function render(){
+    ReactDOM.render(
+    <Apps />,
+    document.getElementById('app')
+    );
+}
+render()
