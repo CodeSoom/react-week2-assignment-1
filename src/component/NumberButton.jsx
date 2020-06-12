1606f147cba66a59e5a0c1e38dbc7bec3e232eac
aumilkyeong <aumilkyeong@gmail.com>
2020-06-12 10:13:10 +0900

    깃헙 CI 테스트 실패로 인한 파일 제거

--- src/component/NumberButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function NumberButton({ number, onClick }) {
-  return (
-    <button type="button" onClick={onClick}>
-      {number}
-    </button>
-  );
-}
