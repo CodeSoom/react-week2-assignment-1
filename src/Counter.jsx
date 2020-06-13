48d31a36382f6ba7127bed080e70955c8c2c4557
Paul An <anpaul0615@gmail.com>
2020-06-14 02:00:23 +0900

    "2주차 과제 1 - Counter 앱 만들고 파일 분리하기" 최초구현

---
+++ src/Counter.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Counter</p>
+      <p>
+        <button type="button" onClick={() => setCount(count + 1)}>
+          {`Click me (${count})`}
+        </button>
+      </p>
+      <p>
+        {
+          [1, 2, 3, 4, 5].map((number) => (
+            <button type="button" onClick={() => setCount(count + number)}>
+              {number}
+            </button>
+          ))
+        }
+      </p>
+    </div>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Counter from './Counter';
+
+
+ReactDOM.render(
+  <Counter />,
+  document.getElementById('app'),
+);
