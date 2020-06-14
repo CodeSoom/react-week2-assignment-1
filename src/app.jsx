d3b2591f797e8c7c97bbb546879b2ec1e6709515
wjdskagjs1 <wjdskagjs1@naver.com>
2020-06-14 18:28:26 +0900

    accept advice

--- src/app.jsx
+++ src/app.jsx
@@ -4,16 +4,17 @@ import Button from './Button';
 
 export default function App() {
   const [state, setState] = useState({ count: 0 });
-  const handlers = [1, 1, 2, 3, 4, 5].map(
-    (number) => () => setState({ count: state.count + number }),
-  );
-  const mapButton = (number) => <Button value={number} onClick={handlers[number]} />;
+  const handleClick = (number) => () => setState({ count: state.count + number });
 
   return (
     <div>
       <div>Counter</div>
-      <div><Button value={`Click me! (${state.count})`} onClick={handlers[0]} /></div>
-      {[1, 2, 3, 4, 5].map(mapButton)}
+      <div>
+        <Button value={`Click me! (${state.count})`} onClick={handleClick} />
+      </div>
+      {[1, 2, 3, 4, 5].map((number) => (
+        <Button number={number} onClick={handleClick} />
+      ))}
     </div>
   );
 }
