3f94dd28a98f91da018a25be24ea8a6bb0597ec6
amorfati0310 <lyhy0310@gmail.com>
2020-06-16 02:56:37 +0900

    props name 변경 안 한 부분 다시 적용 [Ticket: X]

--- src/page/CounterPage.jsx
+++ src/page/CounterPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Button from '../components/Button';
 import CounterButtons from '../components/CounterButtons';
 
-export default function CounterPage({ onClick, buttonList, count }) {
+export default function CounterPage({ onClick, buttons, count }) {
   const BaseIncreasement = 1;
   return (
     <div>
@@ -12,7 +12,7 @@ export default function CounterPage({ onClick, buttonList, count }) {
       </Button>
       <div>
         <CounterButtons
-          buttonList={buttonList}
+          buttons={buttons}
           onClick={onClick}
         />
       </div>
