c28de580ab47b78f0e5a253916516963d7abb126
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-21 11:17:30 +0900

    initial index.jsx
    initial index.jsx from lecture.

---
+++ src/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function Counter({ count, onClick }) {
+  return (
+    <button type="button" onClick={onClick}>
+      Click Me! (
+      {count}
+      )
+    </button>
+  );
+}
+
+function Button({ children }) {
+  return (
+    <button type="button">
+      {children}
+    </button>
+  );
+}
+
+function Buttons() {
+  return (
+    <p>
+      {[1, 2, 3, 4, 5].map((i) => (
+        <Button key={i}>
+          {i}
+        </Button>
+      ))}
+    </p>
+  );
+}
+function Page({ count, onClick }) {
+  return (
+    <div>
+      <p>Counters</p>
+      <Counter count={count} onClick={onClick} />
+      <Buttons />
+    </div>
+  );
+}
+
+function App() {
+  const [state, setState] = useState({
+    count: 0,
+  });
+
+  const { count } = state;
+
+  function handleClick() {
+    setState({
+      count: count + 1,
+    });
+  }
+  return (
+    <Page count={count} onClick={handleClick} />
+  );
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#app'));
