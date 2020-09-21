64c4898e22dc2e86ab5410249cb289ec983dd2d9
juunini <juuni.ni.i@gmail.com>
2020-09-21 22:31:00 +0900

    Make sample view

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 function App() {
   return (
     <div>
-      Hello, World!
+      <p>Counter</p>
+      <p>
+        <button type="button">Click me! (0)</button>
+      </p>
+      <p>
+        {
+          [1, 2, 3, 4, 5].map((i) => (
+            <button type="button">{i}</button>
+          ))
+        }
+      </p>
     </div>
   );
 }
