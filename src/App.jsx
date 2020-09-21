c873c1926062f3b8df9f76ebb78bb560c640e95f
gringrape <53764714+gringrape@users.noreply.github.com>
2020-09-21 20:23:17 +0900

    Add 'count' state and render it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [state] = useState({
+    count: 123,
+  });
+
+  const { count } = state;
+
   return (
     <div>
-      <p>App is working!</p>
+      <p>Counter</p>
+      <p>
+        count:(
+        {count}
+        )
+      </p>
     </div>
   );
 }
