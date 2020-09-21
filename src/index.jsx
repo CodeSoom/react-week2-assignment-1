6f488f9ae005f9742a721914c35fc6f4053cd975
saseungmin <dbd02169@naver.com>
2020-09-21 20:04:25 +0900

    Update Divide Components
    - Component 별 파일로 분리
    - Counter 기능 업데이트

--- src/index.jsx
+++ src/index.jsx
@@ -1,51 +1,15 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import Page from './components/Page';
 
-
-function Counter({ count, onClick }) {
-  return (
-    <button type="button" onClick={onClick}>
-      Click me!
-      (
-      {count}
-      )
-    </button>
-  );
-}
-
-function Button({ children }) {
-  return (
-    <button type="button">{children}</button>
-  );
-}
-
-function Buttons() {
-  return (
-    <p>
-      {[1, 2, 3].map((i) => (
-        <Button key={i}>{i}</Button>
-      ))}
-    </p>
-  );
-}
-
-function Page({ count, onClick }) {
-  return (
-    <div>
-      <p>Hello, world!</p>
-      <Counter count={count} onClick={onClick} />
-      <Buttons />
-    </div>
-  );
-}
 
 function App() {
   const [state, setState] = useState({ count: 0 });
 
   const { count } = state;
 
-  function handleClick() {
-    setState({ count: count + 1 });
+  function handleClick(value = 1) {
+    setState({ count: count + value });
   }
 
   return (
