dece1acaa2b591784eb7acb790ce5d0fed4bd162
hyeonhyejin <dev.hyejin04@gmail.com>
2020-09-21 23:57:34 +0900

    Implement clickListener of buttons

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const numbers = (upTo) => Array.from({ length: upTo }, (_, i) => i + 1);
 
+const countInSteps = (origin, step) => origin + step;
+
 function App() {
+  const [state, setState] = useState({
+    count: 0,
+  });
+
+  const { count } = state;
+
+  function handleCounterButton(step) {
+    setState({
+      count: countInSteps(count, step),
+    });
+  }
+
   return (
     <div>
       <p>
         Counter
       </p>
-      <button type="button">
-        Click Me!
+      <button type="button" onClick={() => handleCounterButton(1)}>
+        Click me!
         (
-        {1}
+        {count}
         )
       </button>
 
       <div>
         {
           numbers(5).map((i) => (
-            <button type="button">
+            <button key={i} type="button" onClick={() => handleCounterButton(i)}>
               {i}
             </button>
           ))
