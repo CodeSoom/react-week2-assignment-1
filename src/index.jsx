9527a23dbe4cff88906773bb9ce80aace3bdd412
hyeonhyejin <dev.hyejin04@gmail.com>
2020-09-21 23:06:43 +0900

    Init the project
    - Add webpack-dev-server
    - Add the App component

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App(){
+    return(
+        <div>
+            <p>hello world</p>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+);
