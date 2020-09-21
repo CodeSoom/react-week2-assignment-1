70aa3adc60dd747587b491beddd0fe64800d4ef1
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-21 12:36:36 +0900

    Separate Component from index.jsx.
    Create each Component file to separate component from index.jsx.

---
+++ src/Page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './Counter';
+import Buttons from './Buttons';
+
+function Page({ count, onClick }) {
+  return (
+    <div>
+      <p>Counter</p>
+      <Counter count={count} onClick={() => onClick(1)} />
+      <Buttons onClick={onClick} />
+    </div>
+  );
+}
+
+export default Page;
