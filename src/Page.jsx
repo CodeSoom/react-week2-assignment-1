7c27ec6742e099a7f5bb8d8d4a5ade4bf1e88306
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-21 15:07:46 +0900

    Refactoring : add count button
    Tried Add count button to reuse button component.

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Counter from './Counter';
 import Buttons from './Buttons';
+import Button from './Button';
 
 function Page({ count, onClick }) {
   return (
     <div>
       <p>Counter</p>
       <Counter count={count} onClick={() => onClick(1)} />
+      <Button onClick={() => onClick(1)}>
+        {`count : ${count}`}
+      </Button>
       <Buttons onClick={onClick} />
     </div>
   );
