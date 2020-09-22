327c5b880c0e4c482352a0350fd167388208dcb9
Dokyun Lim <im.koremp@gmail.com>
2020-09-23 01:14:02 +0900

    Update src/index.js for initial

---
+++ src/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function Counter({count, onClick}) {
+    return (
+        <button type='button' onClick={onClick}>
+            Click Me!
+            {
+                count
+            }
+        </button>
+    )
+}
+
+function Button({ children }) {
+    return (
+        <button type="button">
+            {children}
+        </button>
+    )
+}
+
+function Buttons() {
+    return (
+        <p>
+            {[1,2,3].map((i) => {
+                <Button key={i}>
+                    {i}
+                </Button>
+            })}
+        </p>
+    )
+}
+
+function App() {
+    const [state, setState] = useState({
+        count: 0,
+    });
+
+    const {count} = state;
+
+    function handleClick() {
+        setState({
+            count: count + 1,
+        });
+    }
+
+    return (
+        <div>
+            <p>Hello, World!</p>
+            <p>Hi!</p>
+            <Counter
+                count={count}
+                onClick={handleClick}
+            />
+            <Buttons />
+        </div>
+    )
+}
+
+ReactDOM.render(App(), document.getElementById('app'));
