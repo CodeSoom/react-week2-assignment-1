ef340160a63636cf806f35fdbbbcafb6bd5e0492
Seungmin Lee <seungmin@uxplus.dev>
2020-09-22 11:16:47 +0900

    Componentize `App`
    Componentize `App` to separate concerns.

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Page from './Page';
-
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  function handleClick({ difference }) {
-    setCount(count + difference);
-  }
-
-  return (
-    <Page count={count} onClick={handleClick} />
-  );
-};
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
