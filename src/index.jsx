66ebf6be228b520e14bf8103798b3842f1bbd86a
dev cloubot <goodzzong@gmail.com>
2020-09-22 13:52:05 +0900

    Add 숫자 1 버튼 클릭시 숫자가 1씩 증가
    Create handleClickNumber Function

--- src/index.jsx
+++ src/index.jsx
@@ -13,6 +13,13 @@ function App() {
       count: count + 1,
     });
   };
+
+  const handleClickNumber = () => {
+    setState({
+      count: count + 1,
+    });
+  };
+
   return (
     <div>
       <p>Counter</p>
@@ -22,7 +29,9 @@ function App() {
         </button>
       </p>
       <p>
-        <button type="button">1</button>
+        <button type="button" onClick={() => handleClickNumber()}>
+          1
+        </button>
         <button type="button">2</button>
         <button type="button">3</button>
         <button type="button">4</button>
