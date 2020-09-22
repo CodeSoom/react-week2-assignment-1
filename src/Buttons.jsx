c6e8383d48dce0ce7ac9022afa47819a37dfa533
dev cloubot <goodzzong@gmail.com>
2020-09-22 14:32:32 +0900

    refactoring Buttons 컴포넌트의 버튼 분리
    Create Button component

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import Button from './Button';
 
 function Buttons({ onClick }) {
   return (
     <p>
       {[1, 2, 3, 4, 5].map((i) => (
-        <button key={i} type="button" onClick={() => onClick(i)}>
-          {i}
-        </button>
+        <Button key={i} number={i} onClick={onClick} />
       ))}
     </p>
   );
