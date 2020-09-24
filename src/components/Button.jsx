3593898975255176e4b9be3f8af816888783b6b0
comejtome508 <youngnoh.goh@gmail.com>
2020-09-25 01:27:16 +0900

    Combine CounterBtn and Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-export default function Button({ count, onClick }) {
+export default function Button({ children, count, onClick }) {
   return (
-    <button type="button" onClick={() => onClick(1)}>
-      Click me!
-      (
-      {count}
-      )
-    </button>
+   <div>
+      <button type="button" onClick={() => onClick(1)}>
+        Click me!
+        (
+        {count}
+        )
+        {children}
+      </button>
+    </div>
   );
 }
