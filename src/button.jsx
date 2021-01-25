60374ac2574486c3472f0e56ec8e2631a5aab13d
shinhyogeun <tls1gy2rms3@naver.com>
2021-01-25 20:10:42 +0900

    refactor : ESLint에 따른 수정

--- src/button.jsx
+++ src/button.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-export function Button({ count, onClick }) {
-    return (
-        <p><button onClick={onClick}>Click me!({count})</button></p>
-    )
+export default function Button({ count, onClick }) {
+  return (
+    <p>
+      <button type="button" onClick={onClick}>
+        Click me!(
+        {count}
+        )
+      </button>
+    </p>
+  );
 }
