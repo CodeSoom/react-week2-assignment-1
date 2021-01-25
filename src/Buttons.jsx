eb32865c09816357d1d7c340ebf9c91e38c597c5
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-01-26 02:58:25 +0900

    fix auto formating - 이제는 되겠지

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 function Buttons({ count, setFunction }) {
-	return (
-		<p>
-			{[1, 2, 3, 4, 5].map((i) => (
-				<button key={i} type="button" onClick={() => setFunction(count + i)}>
-					{i}
-				</button>
-			))}
-		</p>
-	);
+  return (
+    <p>
+      {[1, 2, 3, 4, 5].map((i) => (
+        <button key={i} type="button" onClick={() => setFunction(count + i)}>
+          {i}
+        </button>
+      ))}
+    </p>
+  );
 }
 
 export default Buttons;
--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,15 @@ import Counter from './Counter';
 import Buttons from './Buttons';
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	return (
-		<div>
-			<p>Counter</p>
-			<Counter count={count} onClick={()=> setCount(count + 1)}/>
-			<Buttons count={count} setFunction={setCount}/>
-		</div>
-	);
+  return (
+    <div>
+      <p>Counter</p>
+      <Counter count={count} onClick={() => setCount(count + 1)} />
+      <Buttons count={count} setFunction={setCount} />
+    </div>
+  );
 }
 
 
