822298fe95f50e32412d13d026a8a5088bf4aa2f
good <good@bsidesoft.com>
2021-01-26 23:03:24 +0900

    codecept URL 8080으로 변경

--- codecept.conf.js
+++ codecept.conf.js
@@ -9,7 +9,7 @@ exports.config = {
   output: './output',
   helpers: {
     Puppeteer: {
-      url: 'http://localhost:808',
+      url: 'http://localhost:8080',
       show: true,
       windowSize: '1200x900',
     },
