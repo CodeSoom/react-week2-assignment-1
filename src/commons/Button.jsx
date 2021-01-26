2a394538e6412ddacc6ed2c54a47517a1379d658
dodoongtak <themighty.tak@gmail.com>
2021-01-27 05:35:19 +0900

    Update function arguments
    Rather destructuring inside the function, receiving as an object destructred at initial

--- src/commons/Button.jsx
+++ src/commons/Button.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-function Button(props) {
-  const { onClick, children } = props;
+function Button({ onClick, children }) {
   return (
     <button
       type="button"
