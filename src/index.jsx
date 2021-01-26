4c46b8a92ede2ebfee376bcf256bb78e8fe2d64d
younghoon <secret9179@gmail.com>
2021-01-26 18:37:53 +0900

    Simple Counter App

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDom from 'react-dom';
+
+function Counter({ count, onClick }) {
+    return (
+        <button type="button" onClick={onClick}>
+            Click me!
+            (
+            { count }
+            )
+        </button>
+    );
+}
 
 function Button({children}) {
     return (
@@ -9,30 +20,68 @@ function Button({children}) {
     );
 }
 
-function Buttons() {
+function Buttons({ onClick }) {
     return (
         <p>
-            {[1, 2, 3].map(i => (
-            <Button key={i}>
-                {i}
-            </Button>
-        ))}
+            {[1, 2, 3, 4, 5].map(i => <button type='button' key={i} onClick={() => onClick(i)}>{i}</button>)}
         </p>
     );
 }
 
-function App() {
+function Page({count, onclick}){
     return (
-    <div>
-        <p>Hello, world!!</p>
-        <p>hi!</p>
-        <Buttons />
-    </div>
-  );
+        <div>
+            <Counter
+                count={count}
+                onClick={onclick}
+            />
+        </div>
+    );
 }
 
+function PageNum({count, onclick}){
+    return (
+        <div>
+            <Buttons
+                count={count}
+                onClick={onclick}
+            />
+        </div>
+    );
+}
+
+function App() {
+    const [state, setState] = useState({
+        count: 0,
+    });
+
+    const {count} = state;
+
+    function handleClick(){
+        setState({
+            count: count + 1,
+        });
+    }
+
+    function numClick(i){
+        setState({
+            count: count + i,
+        });
+    }
+
+    return (
+        <div>
+            <p>Counter</p>
+            <Page
+                count={count}
+                onclick={handleClick}
+            />
+            <Buttons onClick={numClick} />
+        </div>     
+    )
+}
 
 ReactDom.render (
     <App />,
-    document.getElementById('app')
+    document.getElementById('app'),
 );
