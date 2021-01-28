03281ebe0f2ddfba25e893d63301ba57d5464136
gimsesu <tkfrn4799@gmail.com>
2021-01-28 21:05:54 +0900

    Create Buttons
    Click me 버튼을 구현했습니다.
    숫자 버튼들을 구현했습니다.
    숫자 버튼을 구현할 때, Button(i) 대신 좀 더 좋은 방법이 있는지 고민해봐야
    할 것 같습니다.

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const element = (
-  <div>
-    hello
-  </div>
-);
+function Title() {
+  return <div>Counter</div>;
+}
+
+function ButtonClickme() {
+  const [count, setCount] = useState(0);
+  return (
+    <button type="button" onClick={() => { setCount(count + 1); }}>
+      Click me!
+      (
+      {count}
+      )
+    </button>
+  );
+}
+
+function Button(value) {
+  return (
+    <button type="button" key={value}>
+      {value}
+    </button>
+  );
+}
+
+function ButtonNumber() {
+  return (
+    [1, 2, 3, 4, 5].map((i) => (
+      Button(i)
+    )));
+}
 
 function App() {
-  return element;
+  return (
+    <div>
+      <Title />
+      <p>
+        <ButtonClickme />
+      </p>
+      <p>
+        <ButtonNumber />
+      </p>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
