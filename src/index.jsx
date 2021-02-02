78e76f51a5be14b6c1c1a27ef65004ec33c16b98
Najeong Kim <atpeoe1023@gmail.com>
2021-02-03 00:42:22 +0900

    answer code

--- src/index.jsx
+++ src/index.jsx
@@ -1,67 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom'
 
-import Counter from './Counter.jsx';
-
-function Button({ children, onClick }) {
-  return (
-    <button type="button" onClick={onClick}>
-      {children}
-    </button>
-  )
-}
-
-function Buttons() {
-  return (
-    <p>
-      {[1, 2, 3, 4, 5].map((i) => (
-        <Button key={i}>
-          {i}
-        </Button>
-      ))}
-    </p>
-  )
-}
-
-function App() {
-  const [state, setState] = useState({
-    count: 0,
-  });
-
-  const { count } = state;
-
-  function handleClick() {
-    setState({
-      count: count + 1
-      
-    });
-  }
-
-  function Page({ count, onClick }) {
-    return (
-      <div>
-        <p>Counter</p>
-        <Counter
-          count={count}
-          onClick={onClick}
-        />
-        <Buttons 
-          children={count}
-          onClick={onClick}
-        />
-      </div>
-    )
-  }
-
-  console.log(count)
-
-  return (
-    <Page
-      count={count}
-      onClick={handleClick}
-    />
-  )
-}
+import App from './App'
 
 ReactDOM.render(
   <App />,
