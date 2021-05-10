041f1e1ab0843f7ee305c384c36409f1022cd685
yujonglee <yujonglee@g.skku.edu>
2021-05-10 09:43:33 +0900

    app.jsx로부터 Counter컴포넌트를 분리하라

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,8 @@
 import React from 'react';
+import Counter from './Counter';
 
 export default function App() {
   return (
-    <p>
-      <h1>Counter</h1>
-
-      <p>
-        <button type="button">
-          Click me!
-        </button>
-      </p>
-
-      {[1, 2, 3, 4, 5].map((value) => (
-        <button type="button">
-          {value}
-        </button>
-      ))}
-    </p>
+    <Counter />
   );
 }
