b0e9e456ebd8ced536bb2abfa38642037f6a29d6
yujonglee <yujonglee@g.skku.edu>
2021-05-10 09:40:41 +0900

    app.jsx에 기본 레이아웃 코드를 작성하라

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 export default function App() {
   return (
     <p>
-      hi
+      <h1>Counter</h1>
+
+      <p>
+        <button type="button">
+          Click me!
+        </button>
+      </p>
+
+      {[1, 2, 3, 4, 5].map((value) => (
+        <button type="button">
+          {value}
+        </button>
+      ))}
     </p>
   );
 }
