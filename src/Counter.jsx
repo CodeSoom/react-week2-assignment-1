794f8f7eb5954c183f35d3a5d6f9e86e8bea3719
seong-dodo <dyrnxm52@hanmail.net>
2021-05-10 17:06:32 +0900

    관심사 분리 2
    - Counter 컴포넌트 만들고 관심사 분리

---
+++ src/Counter.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Counter({ count, onClick }) {
+    return (
+        <p>
+            <button
+                type='button'
+                onClick={() => onClick(1)}>
+                Click me!
+                (
+                {count}
+                )
+            </button>
+        </p>
+    );
+}
+
+export default Counter;
--- src/Page.jsx
+++ src/Page.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
-
+import Counter from './Counter.jsx';
 
 function Page({ count, onClick }) {   
     return (
         <div>
         <p>Counter</p>
-        <p>
-            <button
-            type='button'
-            onClick={ () => onClick(1) }>
-                Click me!
-                (
-                { count }
-                )
-            </button>
-        </p>
+        <Counter
+        count={count}
+        onClick={onClick} />
         <p>
             {[1, 2, 3, 4, 5].map((i) => (
                 <button
