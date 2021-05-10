925849bdb16199d3c1f57aa6bf6d27700e22aa33
seong-dodo <dyrnxm52@hanmail.net>
2021-05-10 13:10:18 +0900

    컴포넌트 만들기
    - ClickMeButton 컴포넌트 만들기
    - NumberButton, NumberButtons 컴포넌트 만들기

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,42 @@
 /* JSX React.createElement */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
+
+function ClickMeButton() {
+    return (
+        <p>
+          <button type="button">
+                Click me!
+          </button>
+        </p>
+    );
+}
+
+function NumberButton(props) {
+    return (
+        <button type="button">
+            {props.value}
+        </button>
+    );
+}
+
+function NumberButtons() {
+    return (
+        <p>
+            {[1, 2, 3, 4, 5].map((value) => (
+                <NumberButton value={value} />
+            ))}
+        </p>
+    );
+}
 
 function App() {
     return (
         <div>
-            <p>Hello, World!!!</p>
+            <p>Counter</p>
+            <ClickMeButton />
+            <NumberButtons />
         </div>
     );
 }
