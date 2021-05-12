3a5a3b4f9398b9a5e11e2f3cfa2840d01c900bec
dngwoodo <dngwoodo@gmail.com>
2021-05-12 21:03:45 +0900

    refactor: Add Buttons component

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,21 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Buttons = ({onHandleClickNumberBtn}) => (
+  <>
+    {[1,2,3,4,5].map(number => (
+      <button type="button" onClick={() => onHandleClickNumberBtn(number)}>
+        {number}
+      </button>
+    ))}
+  </>
+)
+
 const App = () => {
   const [count, setCount] = useState(0);
+  const handleClickNumberBtn = (number) => {
+    setCount(count + number)
+  }
   return (
     <div>
       <h2>Counter</h2>
@@ -11,11 +24,7 @@ const App = () => {
       }}>Click! ({count})</button>
       <br />
       <br />
-      {[1,2,3,4,5].map(number => (
-        <button type="button" onClick={() => {
-          setCount(count + number)
-        }}>{number}</button>
-      ))}
+      <Buttons onHandleClickNumberBtn={handleClickNumberBtn}/>
     </div>
   )
 }
