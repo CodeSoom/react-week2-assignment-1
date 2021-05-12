5183b3bb59fd8c90641122a38afb345d2a800c5b
dngwoodo <dngwoodo@gmail.com>
2021-05-12 21:14:28 +0900

    fix: Arrange code

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import Button from './Button';
 import Buttons from './Buttons';
 
@@ -7,22 +7,22 @@ const App = () => {
   const [count, setCount] = useState(0);
 
   const handleClickBtn = () => {
-    setCount(count + 1)
-  }
+    setCount(count + 1);
+  };
 
   const handleClickNumberBtn = (number) => {
-    setCount(count + number)
-  }
+    setCount(count + number);
+  };
 
   return (
     <div>
       <h2>Counter</h2>
-      <Button counter={count} onHandleClickBtn={handleClickBtn} />  
+      <Button counter={count} onHandleClickBtn={handleClickBtn} />
       <br />
       <br />
-      <Buttons onHandleClickNumberBtn={handleClickNumberBtn}/>
+      <Buttons onHandleClickNumberBtn={handleClickNumberBtn} />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.querySelector('#app'))
+ReactDOM.render(<App />, document.querySelector('#app'));
