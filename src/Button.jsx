4ac8803ae6bda8e0b92a6c773ed7b3c1dfa5ce9f
dngwoodo <dngwoodo@gmail.com>
2021-05-13 01:44:31 +0900

    fix: Change button children for test

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Button = ({ count, handleClickBtn }) => (
   <button type="button" onClick={handleClickBtn}>
-    Click! (
+    Click me! (
     {count}
     )
   </button>
