31f621300b7e9d0ed75e9c81379ba87f324a719f
dngwoodo <dngwoodo@gmail.com>
2021-05-12 20:52:51 +0900

    feat: Add number to count when numberic button is clicked

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => (
-  <div>
-    <h2>Counter</h2>
-    <button type="button">Click! (0)</button>
-    <br />
-    <br />
-    {[1,2,3,4,5].map(number => (
-      <button type="button">{number}</button>
-    ))}
-  </div>
-)
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2>Counter</h2>
+      <button type="button">Click! ({count})</button>
+      <br />
+      <br />
+      {[1,2,3,4,5].map(number => (
+        <button type="button" onClick={() => {
+          setCount(count + number)
+        }}>{number}</button>
+      ))}
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.querySelector('#app'))
