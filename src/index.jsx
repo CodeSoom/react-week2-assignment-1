d64a45fef448b316d28b97d435efa1ce6d3f62ed
dngwoodo <dngwoodo@gmail.com>
2021-05-12 20:46:59 +0900

    feat: Add counter app ui

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom'
 
 const App = () => (
   <div>
-    App
+    <h2>Counter</h2>
+    <button type="button">Click! (0)</button>
+    <br />
+    <br />
+    {[1,2,3,4,5].map(number => (
+      <button type="button">{number}</button>
+    ))}
   </div>
 )
 
