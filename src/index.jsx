624168c5446ad1816311f153c8ef651aaee7feea
studioLuda <studio.luda.work@gmail.com>
2021-05-12 09:54:11 +0900

    리액트 과제 2-1 : 카운터 만들기

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// 리액트 2-1 과제 : 첫 커밋
+
+function App() {
+  return (<p>Counter</p>);
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
