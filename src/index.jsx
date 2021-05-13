99f34177a13e58e33c465645f9fca2eaa06f82af
hanvit <hanvit7@gmail.com>
2021-05-13 10:26:00 +0900

    count state 리펙토링
    오브젝트에서 count 단일 변수로 변경
    handleClick 매개변수명 변경

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,11 @@ import ClickMe from './Components/ClickMe';
 import NumberPad from './Components/NumbersPad';
 
 function App() {
-  const [state, setState] = useState({ count: 0 });
-  const { count } = state;
+  const [count, setCount] = useState(0);
   const numbers = [1, 2, 3, 4, 5];
 
-  function handleClick(addNumber) {
-    setState({ count: count + addNumber });
+  function handleClick(number) {
+    setCount(count + number);
   }
 
   return (
