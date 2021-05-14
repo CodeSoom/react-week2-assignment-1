e7e76507aab370b2c8447922bd94583c890dd73c
최한울 <hanul.choi95@nts-corp.com>
2021-05-14 21:09:51 +0900

    [2주차] 2개 이상의 props를 가진 컴포넌트 줄바꿈
    - 2개 이상의 props를 가진 컴포넌트는 줄바꿈을 통해 가독성을 확보한다.

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default function Counter({ count, onClickPlusOne }) {
   return (
     <div>
-      <button type="button" onClick={onClickPlusOne}>
+      <button
+        type="button"
+        onClick={onClickPlusOne}
+      >
         Click me!
         (
         {count}
