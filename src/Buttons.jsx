48a414744b9f64680f87419ede98f14d2f42461d
hxunz <guswl2843@gmail.com>
2021-05-15 18:15:15 +0900

    코드 리팩터링

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import Button from './Button';
 
 function Buttons({ onClick }) {
+  const numbers = [1, 2, 3, 4, 5];
+
   return (
     <p>
-      {[1, 2, 3, 4, 5].map((i) => (
-        <Button key={i} onClick={() => onClick(i)}>
-          {i}
+      {numbers.map((number) => (
+        <Button key={number} onClick={() => onClick(number)}>
+          {number}
         </Button>
       ))}
     </p>
