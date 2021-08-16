62ebe56c0912fe4cc2a7330722094928eb61b159
passwd10 <parkinseo94@naver.com>
2021-08-16 20:56:18 +0900

    refactor: count에 대한 상태를 Page에서 관리한다

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Page from './Page';
 
 export default function App() {
-  const [state, setState] = useState({
-    count: 0,
-  });
-
-  const { count } = state;
-
-  function handleClick() {
-    setState({
-      count: count + 1,
-    });
-  }
-
   return (
-    <Page
-      count={count}
-      onClick={handleClick}
-    />
+    <Page />
   );
 }
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Counter from './Counter';
 import Buttons from './Buttons';
 
-export default function Page({ count, onClick }) {
+export default function Page() {
+  const [state, setState] = useState({
+    count: 0,
+  });
+
+  const { count } = state;
+
+  function handleClick() {
+    setState({
+      count: count + 1,
+    });
+  }
+
   return (
     <div>
       <p>Hello World!</p>
       <Counter
         count={count}
-        onClick={onClick}
+        onClick={handleClick}
       />
       <Buttons />
     </div>
