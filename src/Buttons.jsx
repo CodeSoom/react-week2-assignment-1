8a451d6fd72a7b62c4dffb5c5e022143893d2b22
Sehyun Chung <sehyun.chung@icloud.com>
2021-08-16 20:41:06 +0900

    refactor: add `BUTTON_NUMBERS` const

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 
 import Button from './Button';
 
+const BUTTON_NUMBERS = [1, 2, 3, 4, 5];
+
 export default function Buttons({ setCount }) {
   const onClick = (number) => setCount((prevCount) => prevCount + number);
   return (
     <>
-      {[1, 2, 3, 4, 5].map((number) => <Button onClick={() => onClick(number)}>{number}</Button>)}
+      {BUTTON_NUMBERS.map((number) => <Button onClick={() => onClick(number)}>{number}</Button>)}
     </>
   );
 }
