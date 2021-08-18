ce482f818e603ae0af4956a5b8cb67c3793ea086
developerOlive <chloehiho@gmail.com>
2021-08-18 20:10:47 +0900

    [리팩토링] 컴포넌트 분리 - App

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Page from './Page';
+
+export default function App() {
+    const [state, setState]  = useState({
+        count: 0,
+    });
+    
+    const { count } = state;
+
+    function handleClick(i) {
+        setState({
+            count: count + i,
+        });
+    }
+    
+    return (
+        <Page
+            count={count}
+            onClick={handleClick}
+        />
+    );
+}
