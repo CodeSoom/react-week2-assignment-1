d0902da4b99ce816fcac0d589475ac860027b7cc
developerOlive <chloehiho@gmail.com>
2021-08-18 20:11:01 +0900

    [리팩토링] 컴포넌트 분리 - Page

---
+++ src/Page.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Counter from './Counter';
+import Buttons from './Buttons';
+
+export default function Page({ count, onClick }) {
+    return (
+        <div>
+            <p>Hello, Olive!</p>
+            <Counter 
+                count={count}
+                onClick={onClick}
+            />
+            <Buttons
+                onClick={onClick}
+            />
+        </div>
+    );
+}
