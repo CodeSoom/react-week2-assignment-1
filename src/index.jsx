79da40e0bcfca749c1c91b16371391fc8ce37c63
developerOlive <chloehiho@gmail.com>
2021-08-18 20:11:46 +0900

    [리팩토링] index.js 파일에서 분리한 컴포넌트 삭제, 필요한 import 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,77 +1,8 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-
-function Counter({ count, onClick }) {
-    return (
-        <button type="button" onClick={() => onClick(1)}>
-            Click me!
-            (
-            { count }
-            )
-        </button>
-    );
-}
-
-function Button({ children, onClick }) {
-    return (
-            <button type="button" onClick={onClick}>
-                {children}
-            </button>
-    );
-}
-
-function Buttons({ onClick }) {
-    const NUMBER_BUTTONS = [1, 2, 3, 4, 5];
-
-    return (
-        <p>
-            {NUMBER_BUTTONS.map((i) => (
-                <Button key={i} onClick={() => onClick(i)}>
-                    {i}
-                </Button>
-            ))}
-        </p>
-    );
-}
-
-function Page({ count, onClick }) {
-    return (
-        <div>
-            <p>Hello, world!</p>
-            <Counter 
-                count={count}
-                onClick={onClick}
-            />
-            <Buttons 
-                onClick={onClick}
-            />
-        </div>
-    );
-}
-
-function App() {
-    const [state, setState]  = useState({
-        count: 0,
-    });
-    
-    const { count } = state;
-
-    function handleClick(i) {
-        setState({
-            count: count + i,
-        });
-    }
-    
-    return (
-        <Page
-            count={count}
-            onClick={handleClick}
-        />
-    );
-}
+import App from './App';
 
 ReactDOM.render(
     <App />,   
     document.getElementById('app'),
 );
-
