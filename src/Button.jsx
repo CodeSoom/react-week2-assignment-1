cf3fba3f1eaf6f458c013a9e7371992eff48bb6d
dev-hera <dev.chohera@gmail.com>
2021-08-18 18:57:21 +0900

    Fix: 코드 prettier, comment 삭제

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
-// eslint-disable-next-line react/prop-types
 export default function Button({ children, onClick }) {
   return (
-    <button type="button" onClick={onClick}>{ children }</button>
+    <button
+      type="button"
+      onClick={onClick}
+    >
+      { children }
+    </button>
   );
 }
