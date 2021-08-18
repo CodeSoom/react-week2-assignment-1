69383d84c29a416a2778f4cb012bb924fc690a39
developerOlive <chloehiho@gmail.com>
2021-08-18 21:53:15 +0900

    esLint fix

--- src/Page.jsx
+++ src/Page.jsx
@@ -5,7 +5,7 @@ import Buttons from './Buttons';
 export default function Page({ count, onClick }) {
     return (
         <div>
-            <p>Hello, Olive!</p>
+            <p>Hello, Olive!!!</p>
             <Counter 
                 count={count}
                 onClick={onClick}
