1b4dcf1827e09f8185a288b93841c126899a3f4b
psy082 <zhappy114@gmail.com>
2021-08-20 18:57:32 +0900

    refactor: CountButton 속성 개행

--- src/CountButton.jsx
+++ src/CountButton.jsx
@@ -1,6 +1,9 @@
 export default function CountButton({ count, onClick }) {
   return (
-    <button type="button" onClick={onClick}>
+    <button
+      type="button"
+      onClick={onClick}
+    >
       Click me!
       (
       {count}
