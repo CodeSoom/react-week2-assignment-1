d1fa73425e185979d64494919dcc21c7e6f4c593
CheolWoong Choi <bluesky4381@naver.com>
2021-12-07 01:09:05 +0900

    Arrange app code using Page component

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import Title from './Title';
+import Page from './Page';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,22 +10,11 @@ function App() {
   const handleClickNumber = (number) => setCount((prevCount) => prevCount + number);
 
   return (
-    <div>
-      <Title />
-      <br />
-      <button type="button" onClick={handleClick}>
-        Click me! (
-        {count }
-        )
-      </button>
-      <br />
-      <br />
-      {[1, 2, 3, 4, 5].map((number) => (
-        <button type="button" onClick={() => handleClickNumber(number)}>
-          {number}
-        </button>
-      ))}
-    </div>
+    <Page
+      count={count}
+      handleClick={handleClick}
+      handleClickNumber={handleClickNumber}
+    />
 
   );
 }
