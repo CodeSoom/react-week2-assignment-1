5c0994edbd8365f6679744848e185a5da3e1023b
CheolWoong Choi <bluesky4381@naver.com>
2021-12-07 00:49:32 +0900

    Create app activating all features

---
+++ src/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Title() {
+  return (
+    <div>
+      <div>
+        Counter
+      </div>
+      <br />
+    </div>
+  );
+}
+
+export default Title;
---
+++ src/index.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => setCount((prevCount) => prevCount + 1);
+
+  const handleClickNumber = (number) => setCount((prevCount) => prevCount + number);
+
+  return (
+    <div>
+      <div>
+        Counter
+      </div>
+      <br />
+      <button type="button" onClick={handleClick}>
+        Click me! (
+        {count }
+        )
+      </button>
+      <br />
+      <br />
+      {[1, 2, 3, 4, 5].map((number) => (
+        <button type="button" onClick={() => handleClickNumber(number)}>
+          {number}
+        </button>
+      ))}
+    </div>
+
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
