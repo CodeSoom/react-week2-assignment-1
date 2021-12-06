17f675fff62e776820c1a330a24dce904888b4fe
PAPICO <ocipap0531@gmail.com>
2021-12-06 23:12:48 +0900

    new: handleClick 동작 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick(number) {
+    setCount(count + number);
+  }
+
   return (
     <>
       <p>Counter</p>
       <p>
-        <button type="button">Click me! (0)</button>
+        <button type="button" onClick={() => handleClick(1)}>
+          Click me! (
+          {count}
+          )
+        </button>
       </p>
       <p>
-        {[1, 2, 3, 4, 5].map((i) => <button type="button">{i}</button>)}
+        {[1, 2, 3, 4, 5].map((i) => <button type="button" onClick={() => handleClick(i)}>{i}</button>)}
       </p>
     </>
   );
