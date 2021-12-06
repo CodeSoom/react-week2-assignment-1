ad72487f57cb059680412ceadd8be9883a4b28d8
CheolWoong Choi <bluesky4381@naver.com>
2021-12-07 01:07:45 +0900

    Create Page component

---
+++ src/Page.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import Title from './Title';
+import Button from './Button';
+import Numbers from './Numbers';
+
+function Page({ count, handleClick, handleClickNumber }) {
+  return (
+    <div>
+      <Title title="Counter" />
+      <br />
+      <Button count={count} onClick={handleClick} />
+      <br />
+      <br />
+      <Numbers onClick={handleClickNumber} />
+    </div>
+  );
+}
+
+export default Page;
