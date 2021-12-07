200bff62aff5da1dc8b5687887a2f1836a971870
Superduper-India <sunyoung1542@gmail.com>
2021-12-07 17:24:20 +0900

    Update: Props로 NumericButton컴포넌트 나누기

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function NumericButton() { // (3) NumericButton컴포넌트: App에서 실행됨
+function Button(props) { // (4) Button컴포넌트: App에서 실행됨, i라는 props를 받음
+    const { children } = props
     return (
-        [1, 2, 3, 4, 5].map((i) => (
-            <button>{i}</button>
-        ))
+        <button type="button">
+            {children}
+        </button>
     )
 }
 
-function ClickButton() { // (2) ClickButton컴포넌트: App에서 실행됨
+function Numeric() { // (3) Numeric컴포넌트: App에서 실행됨, i라는 props를 Button컴포넌트에 전달
+    return (
+        <p>
+            {
+                [1, 2, 3, 4, 5].map((i) => (
+                    <Button>{i}</Button>
+                ))
+            }
+        </p>
+    )
+}
+
+function Counter() { // (2) Counter컴포넌트: App에서 실행됨
     return (
         <button>Click me! ()</button>
     )
@@ -19,8 +32,8 @@ function App() { // (1) App컴포넌트: ReactDOM에서 실행됨
     return (
         <div>
             <p>Counter</p>
-            <ClickButton />
-            <NumericButton />
+            <Counter />
+            <Numeric />
         </div>
     )
 }
