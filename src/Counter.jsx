1e7948f8faca3e4d9568b2bc3b7fc62ffb908226
emewjin <emewjin@gmail.com>
2021-12-07 21:43:12 +0900

    up: number 분리

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import NumberButton from './NumberButton';
 import ClickMeButton from './ClickMeButton';
 
+const NUMBERS = [1, 2, 3, 4, 5];
+
 const Counter = ({ count, clickHandler }) => (
   <>
     <h1>Counter</h1>
     <ClickMeButton count={count} clickHandler={() => clickHandler(1)} />
     <div>
-      {[1, 2, 3, 4, 5].map((number) => (
+      {NUMBERS.map((number) => (
         <NumberButton
           key={number}
           number={number}
