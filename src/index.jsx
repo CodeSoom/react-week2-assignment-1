83625146956457519ad666ad20e03b75612366aa
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-07 12:47:31 +0900

    Click Me! 구현

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,27 @@
-console.log('test');
+import React, { Children, useState } from "react";
+import ReactDOM from "react-dom";
+
+
+function Page({ onClick, count }) {
+  return (
+    <button onClick={onClick} >Click Me! ({count})</button>
+  )
+}
+
+function App() {
+  const [state, setState] = useState({
+    count: 0
+  })
+  const { count } = state
+
+  function handleClick() {
+    setState({
+      count: count + 1
+    })
+  }
+
+  return (
+    <Page onClick={handleClick} count={count} />
+  )
+}
+ReactDOM.render(<App />, document.getElementById('app'))
