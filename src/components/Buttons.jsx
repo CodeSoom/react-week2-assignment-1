d5f3053594dcaa10907ff6d5d62d9d42582dee4f
yeji <lerry597@gmail.com>
2021-12-07 22:25:11 +0900

    Modify for test

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import Button from './Button';
 
 function Buttons({ onClick }) {
-	return (
-		<p>
-			{[1, 2, 3, 4, 5].map((i) => (
-				<Button key={i} onClick={() => onClick(i)}>
-					{i}
-				</Button>
-			))}
-		</p>
-	);
+  return (
+    <p>
+      {[1, 2, 3, 4, 5].map((i) => (
+        <Button key={i} onClick={() => onClick(i)}>
+          {i}
+        </Button>
+      ))}
+    </p>
+  );
 }
 
 export default Buttons;
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -4,17 +4,17 @@ import Buttons from './Buttons';
 
 function Page({ count, onClick }) {
   return (
-		<div>
-			<p>Counter</p>
-			<Counter
-				count={count}
-				onClick={onClick}
-			/>
-			<Buttons
-				onClick={onClick}
-			/>
-		</div>
-	);
+    <div>
+      <p>Counter</p>
+      <Counter
+        count={count}
+        onClick={onClick}
+      />
+      <Buttons
+        onClick={onClick}
+      />
+    </div>
+  );
 }
 
 export default Page;
