9aefd2adff775e6b5be502d747b8479e7775f9ea
Superduper-India <sunyoung1542@gmail.com>
2021-12-08 00:29:40 +0900

    Update: Hook(useState)사용, 조건문을 포함한 Condition컴포넌트 생성

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-function Button(props) { // (4) Button컴포넌트: App에서 실행됨, i라는 props를 받음
+function Condition({ children }) { // (5) Condition컴포넌트: (4)으로부터 props의 children을 받고, children으로 조건문실행
+    if (children === 1) {
+        return (
+            console.log(children)
+        )
+    }
+    if (children === 2) {
+        return (
+            console.log(children)
+        )
+    }
+    if (children === 3) {
+        return (
+            console.log(3)
+        )
+    }
+    if (children === 4) {
+        return (
+            console.log(4)
+        )
+    }
+    if (children === 5) {
+        return (
+            console.log(5)
+        )
+    }
+}
+
+function Button(props) { // (4) Button컴포넌트: App에서 실행됨, (3)으로부터 i라는 props를 받고, (5)로 props의 children을 전달
     const { children } = props
     return (
-        <button type="button">
+        <button onClick={() => Condition({ children })}>
             {children}
         </button>
     )
 }
 
-function Numeric() { // (3) Numeric컴포넌트: App에서 실행됨, i라는 props를 Button컴포넌트에 전달
+function Numeric() { // (3) Numeric컴포넌트: App에서 실행됨, (4)로 i라는 props를 전달
     return (
         <p>
             {
@@ -22,17 +50,28 @@ function Numeric() { // (3) Numeric컴포넌트: App에서 실행됨, i라는 pr
     )
 }
 
-function Counter() { // (2) Counter컴포넌트: App에서 실행됨
+function Counter({ count, onClick }) { // (2) Counter컴포넌트: App에서 실행됨, React Hook(useState)를 사용하여 state변수와 이 변수를 갱신할 수 있는 함수를 Return
     return (
-        <button>Click me! ()</button>
+        <button type="button" onClick={onClick}>
+            Click me! ({count})
+        </button>
     )
 }
 
-function App() { // (1) App컴포넌트: ReactDOM에서 실행됨
+function App() { // (1) App컴포넌트: ReactDOM에서 실행됨, count&onClick의 props를 (2)에 넘겨줌
+    const [count, setCount] = useState(0) //? 강의처럼 풀어서 하는 이유...
+
+    function handleClick() {
+        setCount(count + 1)
+    }
+
     return (
         <div>
             <p>Counter</p>
-            <Counter />
+            <Counter
+                count={count}
+                onClick={handleClick}
+            />
             <Numeric />
         </div>
     )
