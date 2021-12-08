c6473b7e736a74200d40a9ae209ec0362f88c2d0
emewjin <emewjin@gmail.com>
2021-12-08 23:53:33 +0900

    fix: NumberButtons 컴포넌트 분리

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,22 +1,12 @@
-import NumberButton from './NumberButton';
 import ClickMeButton from './ClickMeButton';
-
-const NUMBERS = [1, 2, 3, 4, 5];
+import NumberButtons from './NumberButtons';
 
 export default function Counter({ count, clickHandler }) {
   return (
     <>
       <h1>Counter</h1>
       <ClickMeButton count={count} clickHandler={() => clickHandler(1)} />
-      <div>
-        {NUMBERS.map((number) => (
-          <NumberButton
-            key={number}
-            number={number}
-            clickHandler={() => clickHandler(number)}
-          />
-        ))}
-      </div>
+      <NumberButtons clickHandler={clickHandler} />
     </>
   );
 }
