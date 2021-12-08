37af430a4a5c8a11d0a65b437f17520006b25897
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-08 20:04:33 +0900

    [chore] Page를 컴포넌트로 분리

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,4 @@
-function NumButton({ onClick, children }) {
-  return (
-    <button onClick={() => onClick(children)} type='button'>{children}</button>
-  )
-}
-
-function Buttons({ onClick }) {
-  const numbers = [1, 2, 3, 4, 5];
-  return (
-    <div>
-      {nums.map((i) => (
-        <NumButton key={i} onClick={onClick}>{i}</NumButton>
-      ))}
-    </div>
-
-  )
-}
-
-function MainClick({ onClick, count }) {
-  return (
-    <button onClick={() => onClick(1)} >Click me ({count})</button>
-  )
-}
-
-
-function Page({ onClick, count }) {
-  return (
-    <div>
-      <MainClick onClick={onClick} count={count} />
-      <Buttons onClick={onClick} />
-    </div>
-  )
-}
+import Page from './Page/jsx'
 
 function App() {
   const [state, setState] = useState({
