b9a7d51de87875200d49d937818acac88a16970a
emewjin <emewjin@gmail.com>
2021-12-08 23:56:47 +0900

    fix: App 컴포넌트 분리

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,4 @@
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './Counter';
-
-function App() {
-  const [count, setCount] = useState(0);
-  const onClickButton = (number) => setCount(count + number);
-  return <Counter count={count} clickHandler={onClickButton} />;
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
