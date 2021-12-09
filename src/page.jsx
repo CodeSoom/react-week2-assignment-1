5b8a4bbdcb636f3d7c11037b6a594d4bc03f49f5
jnny1017 <jnny1017@gmail.com>
2021-12-09 19:26:45 +0900

    Page컴포넌트에서 비지니스 로직을 처리하도록 수정
    index에 포함되어있던 비지니스 로직을 page컴포넌트에서 처리하도록 수정했습니다.

--- src/page.jsx
+++ src/page.jsx
@@ -1,14 +1,34 @@
+import { useState } from 'react';
+
 import Buttons from './buttons';
 import Counter from './counter';
 
-export default function Page({ count, onClick, onClickButton }) {
+export default function Page() {
+  const [state, setState] = useState({
+    count: 0,
+  });
+
+  const { count } = state;
+
+  function handleClickCounter() {
+    setState({
+      count: count + 1,
+    });
+  }
+
+  function handleClickButton(i) {
+    setState({
+      count: count + i,
+    });
+  }
+
   return (
     <div>
       <Counter
         count={count}
-        onClick={onClick}
+        onClick={handleClickCounter}
       />
-      <Buttons onClickButton={onClickButton} />
     </div>
+      <Buttons onClick={handleClickButton} />
   );
 }
