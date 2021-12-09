11694bdd5ea9e26e033f552417aa8939a39d56a3
CheolWoong Choi <bluesky4381@naver.com>
2021-12-09 21:50:18 +0900

    Create app component and refactor index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,5 @@
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
-
-import Page from './Page';
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  const handleClickCount = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
-  const handleClickNumber = (number) => {
-    setCount((prevCount) => prevCount + number);
-  };
-
-  return (
-    <Page
-      count={count}
-      onClickCount={handleClickCount}
-      onClickNumber={handleClickNumber}
-    />
-  );
-}
+import App from './App';
 
 ReactDOM.render(
   <App />,
