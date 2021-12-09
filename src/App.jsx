3fa5872a3ac3d214c8a9952a133539ffb3636b80
syaring <sarah.k.serim@gmail.com>
2021-12-09 21:48:05 +0900

    Add UI elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
 export default function App() {
+  const count = [1, 2, 3, 4, 5];
+
   return (
     <div>
       Counter
+      <div>
+        <button type="button">
+          click!
+        </button>
+      </div>
+      <div>
+        {count.map((it) => (
+          <button
+            type="button"
+            value={it}
+            key={it}
+          >
+            {it}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
