39da0bb839afe801d936fd48e0284a0ab801f9e4
jeongseop.lee <sjl0614@naver.com>
2021-12-09 15:42:49 +0900

    :recycle: App 컴포넌트 분리

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,6 @@
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-import Page from './Components/Page';
-
-const App = () => {
-  const [state, setState] = useState({
-    count: 0,
-  });
-
-  const { count } = state;
-
-  const handleClick = (step) => {
-    setState({
-      count: count + step,
-    });
-  };
-
-  return (
-    <Page
-      count={count}
-      onClick={handleClick}
-    />
-  );
-};
+import App from './Components/App';
 
 ReactDOM.render(
   <App />,
