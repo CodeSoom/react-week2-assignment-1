06bd2bbcc66052c4cfaf2ab21a3c87e421384ec2
shongs27 <shongs27@naver.com>
2021-12-09 20:20:00 +0900

    Fix - 상태값의 이전값 추가

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import Page from './Page';
+
+export default function App() {
+  const [state, setState] = useState({ count: 0 });
+  const { count } = state;
+  function onClickOne() {
+    setState((prev) => ({ ...prev, count: count + 1 }));
+  }
+  function onClickNumbers(numbers) {
+    setState((prev) => ({ ...prev, count: count + numbers }));
+  }
+  return (
+    <Page count={count} onClickOne={onClickOne} onClickNumbers={onClickNumbers} />
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,4 @@
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import Page from './Page';
-
-function App() {
-  const [state, setState] = useState({ count: 0 });
-  const { count } = state;
-  function onClickOne() {
-    setState({ count: count + 1 });
-  }
-  function onClickNumbers(numbers) {
-    setState({ count: count + numbers });
-  }
-  return (
-    <Page count={count} onClickOne={onClickOne} onClickNumbers={onClickNumbers} />
-  );
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
