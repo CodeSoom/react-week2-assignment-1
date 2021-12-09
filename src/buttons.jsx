bd6c60f695dfc63735985e1bfdb44c5dea2eae3c
jnny1017 <jnny1017@gmail.com>
2021-12-09 21:44:38 +0900

    Button컴포넌트에서 사용한 array변수명을 의미있는 변수명으로 수정

--- src/buttons.jsx
+++ src/buttons.jsx
@@ -1,11 +1,11 @@
 import Button from './button';
 
-const array = [1, 2, 3, 4, 5];
+const buttonValue = [1, 2, 3, 4, 5];
 
 export default function Buttons({ onClick }) {
   return (
     <div>
-      {array.map((i) => (
+      {buttonValue.map((i) => (
         <Button
           key={i}
           onClick={() => { onClick(i); }}
