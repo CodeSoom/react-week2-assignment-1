78b16603545ada541d2dac8dae294e4aa94b8664
Ravi <ravi@inflearn.com>
2021-12-11 17:38:19 +0900

    리뷰 반영
    Button 컴포넌트 개선

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,16 @@
-const Button = ({ type, children, onClick }) => (
-  <button type={type === 'submit' ? 'submit' : 'button'} onClick={onClick}>
-    {children}
-  </button>
-);
-
-export default Button;
+/**
+ * @export
+ * @param {{
+ *  type: 'submit' | 'button',
+ *  children: import('react').ReactElement,
+ *  onClick: import('react').MouseEventHandler,
+ * }} { type, children, onClick }
+ * @return {import('react').ReactElement}
+ */
+export default function Button({ type, children, onClick }) {
+  return (
+    <button type={type === 'submit' ? 'submit' : 'button'} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
