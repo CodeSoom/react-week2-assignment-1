b515adcfbf867b5f9d98022199d75b1411e17f7a
Superduper-India <sunyoung1542@gmail.com>
2021-12-11 16:54:01 +0900

    Update: import React 제거

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "presets": [
-    ["@babel/preset-react", {
-      "runtime": "automatic"
-    }]
-  ]
-}
+  presets: [
+    ['@babel/preset-react', {
+      runtime: 'automatic',
+    }],
+  ],
+};
