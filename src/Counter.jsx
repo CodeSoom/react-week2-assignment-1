dc51520ec94d7141f9c95d37581773ac2b058b70
이찬호 <0407chan@naver.com>
2022-03-28 15:43:55 +0900

    add Counter.jsx

---
+++ src/Counter.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import ClickMeButton from './ClickMeButton';
+import NumberButton from './NumberButton';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function handleClick(number) {
+    setCount(count + number);
+  }
+
+  return (
+    <div>
+      <p>Counter</p>
+      <p>
+        <ClickMeButton count={count} onClick={() => handleClick(1)} />
+      </p>
+      {[1, 2, 3, 4, 5].map((i) => (
+        <NumberButton key={i} number={i} onClick={() => handleClick(i)} />
+      ))}
+    </div>
+  );
+}
+
+export default Counter;
