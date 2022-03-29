54b2447496adb4931b110fcf25829de7ed90bb9b
hdpyo <hudongpyo.dev@gmail.com>
2022-03-29 18:58:56 +0900

    chore: 불필요한 내용 삭제 및 요소 추가
    - Counter <p> 요소 추가

--- src/Page.jsx
+++ src/Page.jsx
@@ -5,8 +5,7 @@ import Buttons from './Buttons';
 export default function Page({ count, onClick }) {
   return (
     <div>
-      <p>Hello, world!!!</p>
-      <p>Hi</p>
+      <p>Counter</p>
       <Counter
         count={count}
         onClick={onClick}
