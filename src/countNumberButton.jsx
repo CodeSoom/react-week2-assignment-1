cbfe2fcab420e61f89d09739595cbc7fb72959c1
kjoobin <kjoobin>
2022-03-29 13:41:50 +0900

    Remove import React
    set babel so doesn't need import react

--- src/countNumberButton.jsx
+++ src/countNumberButton.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function CountNumberButton({ value, onClick }) {
   return (
     <button type="button" onClick={onClick}>{value}</button>
--- src/countPage.jsx
+++ src/countPage.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+
 import CountButton from './CountButton';
 import CountNumberButton from './CountNumberButton';
 
