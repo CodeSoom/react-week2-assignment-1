d227e62106d0ba954284e2d75044a32a1d451407
hdpyo <hudongpyo.dev@gmail.com>
2022-03-29 18:06:24 +0900

    feat: Counter 앱 작성
    [작업 내용]
    - 컴포넌트별로 별도의 파일로 작성.
    
    [TODO]
    - 번호별 버튼 생성 (1~5까지)
    - 버튼에 이벤트 추가 (클릭한 번호의 숫자만큼 더하기)

---
+++ src/Buttons.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Button';
+
+export default function Buttons() {
+  return (
+    <p>
+      {[1, 2, 3].map((i) => (
+        <Button key={i}>
+          {i}
+        </Button>
+      ))}
+    </p>
+  );
+}
