be1edf7011c12b64d7b2ba3125ad344dbfb934b5
hdpyo <hudongpyo.dev@gmail.com>
2022-03-29 18:14:59 +0900

    fix : ESLint 줄바꿈 관련 오류 수정
    [문제]
    - Expected linebreaks to be 'LF' but found 'CRLF’ 에러 발생
    
    [원인]
    - 파일마다 줄바꿈 형식이 LF 형식이 아닌 CRLF 형식으로 작성되어 있었음.
    
    [해결]
    - 각 파일마다 vscode 의 '줄 시퀀스의 끝 선택' 인터페이스에서 'LF' 를 선택함.
    
    [기타]
    - eslint 또는 prettier 의 config 파일을 이용한 해결도 가능하다고 함.
      ㄴ eslint 의 설정 파일을 수정하면 과제의 요구사항에 벗어날 것 같아서 시도하지 않음.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Page from './Page';
 
-export function App() {
+export default function App() {
   const [state, setState] = useState({
     count: 0,
   });
