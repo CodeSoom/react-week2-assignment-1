5b9713b84cef976abb15f5da1ff4c0e10e429d67
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-03-30 07:49:58 +0900

    Revise babel.config.js
    Revise babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = {
   presets: [
     [
       '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
     ],
     [
       '@babel/preset-react',
