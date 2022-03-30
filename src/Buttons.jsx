ee514db6739d14b72cbf43adb26be5c839bdf6c0
HJUN <guswns823@gmail.com>
2022-03-30 21:11:07 +0900

    코드 가독성을 위한 수정

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -5,7 +5,9 @@ const numbers = [1, 2, 3, 4, 5];
 export default function Buttons({ onClick }) {
   return (
     <div>
-      {numbers.map((i) => <Button type="button" key={i} onClick={() => { onClick(i); }}>{i}</Button>)}
+      {numbers.map((i) => (
+        <Button type="button" key={i} onClick={() => onClick(i)}>{i}</Button>
+      ))}
     </div>
   );
 }
