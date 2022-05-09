7f39c30c3a5a0c60fc25fe377861666e0bf20cbb
jaeyooniverse <jaeyoon4819@naver.com>
2022-05-09 11:18:49 +0900

    기능 구현, 테스트, 린트 통과

---
+++ src/ClickmeButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ClickmeButton({ count, onClick }) {
+  return (
+    <p>
+      <button type="button" onClick={onClick}>
+        Click me! (
+        {count}
+        )
+      </button>
+    </p>
+  );
+}
---
+++ src/NumberButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NumberButton({ onClick }) {
+  return (
+    <p>
+      {[1, 2, 3, 4, 5].map((i) => (
+        <button key={i} type="button" onClick={() => onClick(i)}>
+          {i}
+        </button>
+      ))}
+    </p>
+  );
+}
