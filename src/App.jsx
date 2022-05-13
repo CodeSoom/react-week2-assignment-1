74f2c6912f6a2561d38248da3d3b844a090d44a3
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-13 11:46:28 +0900

    테스트코르 에러 수정

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ function App() {
   const [count, setCount] = useState(0);
   const numbers = [1, 2, 3, 4, 5];
 
-  function handleClick(n = 1) {
-    setCount(count + n);
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  function handleNumbersClick(number) {
+    setCount(count + number);
   }
 
   return (
@@ -20,7 +24,7 @@ function App() {
       />
       <Numbers
         numbers={numbers}
-        onClick={handleClick}
+        onClick={handleNumbersClick}
       />
     </div>
   );
