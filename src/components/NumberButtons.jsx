d8a3fa95d1add2bab2609274bcd4d426ded06e19
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-06-13 21:04:57 +0900

    add: 숫자 버튼을 가진 컴포넌트를 추가했습니다

---
+++ src/components/NumberButtons.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function NumberButtons({ countUp }) {
+  const numbers = [1, 2, 3, 4, 5];
+
+  const onClickButton = (number) => {
+    countUp(number);
+  };
+
+  return (
+    <div>
+      {numbers.map((number) => (
+        <button type="button" key={number} onClick={() => onClickButton(number)}>{number}</button>
+      ))}
+    </div>
+  );
+}
+
+export default NumberButtons;
