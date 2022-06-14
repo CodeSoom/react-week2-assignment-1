62e98260fe0569348ed4a4a074ece76faeb9fd18
Woo Dy <kjop7419@naver.com>
2022-06-14 19:59:50 +0900

    refactor > Counter는 제목이기 때문에 h1 태그 사용

--- src/pages/Counter/index.jsx
+++ src/pages/Counter/index.jsx
@@ -5,7 +5,7 @@ import ViewCount from '../../components/ViewCount';
 function Counter({ count, onClick }) {
   return (
     <div>
-      <p>Counter</p>
+      <h1>Counter</h1>
       <ViewCount count={count} onClick={() => onClick()} />
       <Buttons onClick={onClick} />
     </div>
