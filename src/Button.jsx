0c66bc49a39d795b62c6e52d56684d55ad047314
Dave <dave.lee@stagefive.com>
2022-06-15 20:47:20 +0900

    [leehuun][react-week2-assignment-1]  - App, IncrementButtons로 comp 분리  - React babel 설정 추가  - const 변수 리네임  - useCallback 제거

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Button = ({ children, onClick }) => (
   <button type="button" onClick={onClick || null}>
     {children || 'click!'}
--- src/index.jsx
+++ src/index.jsx
@@ -1,32 +1,5 @@
-import React, { useCallback, useState } from 'react';
 import ReactDom from 'react-dom';
-import Button from './Button';
 
-const INCREMENT_LIST = [1, 2, 3, 4, 5];
+import App from './App';
 
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  const handleClick = useCallback((increment) => {
-    setCount((prev) => (prev + increment));
-  }, [count]);
-
-  return (
-    <>
-      <p>Counter</p>
-      <Button onClick={() => handleClick(1)}>
-        {`Click me (${count})`}
-      </Button>
-      <div>
-        {
-          INCREMENT_LIST.map((increment) => (
-            <Button key={`increment-button-${increment}`} onClick={() => handleClick(increment)}>
-              { increment }
-            </Button>
-          ))
-        }
-      </div>
-    </>
-  );
-};
 ReactDom.render(<App />, document.getElementById('app'));
