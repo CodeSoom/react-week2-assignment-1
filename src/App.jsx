f09d69e383a4d6f74d4a33bd10a1656a3eae9ca1
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-06-15 19:33:40 +0900

    refactor: 매직 리터럴을 교체 상수로 교체했습니다

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import NumberButtons from './components/NumberButtons';
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const COUNER_BUTTON_PARAMETER = 1;
+  const NUMBER_BUTTONS_PARAMETER = 5;
 
   const countUp = (number) => {
     setCount(count + Number(number));
@@ -17,8 +19,8 @@ export default function App() {
   return (
     <div>
       <Title />
-      <CountButton increasingNumber={1} count={count} onClick={handleClick} />
-      <NumberButtons increasingNumber={5} onClick={handleClick} />
+      <CountButton increasingNumber={COUNER_BUTTON_PARAMETER} count={count} onClick={handleClick} />
+      <NumberButtons increasingNumber={NUMBER_BUTTONS_PARAMETER} onClick={handleClick} />
     </div>
   );
 }
