91d052900600068e295f3eaef45dccd9c0f7ef5d
moon-kun86 <108505117+moon-kun86@users.noreply.github.com>
2022-07-12 18:06:26 +0900

    Update Buttons.jsx

--- src/component/Buttons.jsx
+++ src/component/Buttons.jsx
@@ -1,20 +1,20 @@
 /* @jsx React.createElement */
 
-import React from 'react'; // 리액트 임포트 필수!
-import Button from './Button';
+import React from "react"; // 리액트 임포트 필수!
+import Button from "./Button";
 
 // 버튼 뷰 컴포넌트
-function Buttons() {
-    return (
-        <p>
-            {[1, 2, 3].map((i) => (
-                // runderButton(i)
-                <Button key = {i}>
-                    {i}
-                </Button>
-            ))}            
-        </p>
-    );
+// eslint-disable-next-line react/prop-types
+function Buttons({ onClick }) {
+  return (
+    <p>
+      {[1, 2, 3, 4, 5].map((i) => (
+        // runderButton(i)
+        // eslint-disable-next-line react/no-children-prop
+        <Button key={i} children={i} onClick={onClick} />
+      ))}
+    </p>
+  );
 }
 
 export default Buttons;
