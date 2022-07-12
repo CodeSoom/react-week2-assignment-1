0d8629b63ffa0ec033c9167a6e69aa581f82ff40
moon-kun86 <108505117+moon-kun86@users.noreply.github.com>
2022-07-12 18:06:52 +0900

    Update Counter.jsx

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -2,9 +2,10 @@
 import React from 'react'; // 리액트 임포트 필수!
 
 // 카운터 컴포넌트
+// eslint-disable-next-line react/prop-types
 function Counter({ count, onClick }){
     return (
-        <button type="button" onClick={onClick}> 
+        <button type="button" onClick={() => onClick()}> 
             Click me! ( { count } )
         </button>
     );
