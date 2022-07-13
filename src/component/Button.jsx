36800246886941a3e55e3de9a9d0490560b7a93b
moon-kun86 <108505117+moon-kun86@users.noreply.github.com>
2022-07-13 11:37:07 +0900

    Update Button.jsx
    export default Button; ->> export default 함수 앞에 선언해주면서 가독성을 높여주었습니다.

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -3,12 +3,10 @@ import React from 'react'; // 리액트 임포트 필수!
 
 // 버튼 컨포넌트
 // eslint-disable-next-line react/prop-types
-function Button({ children, onClick }) {
+export default function Button({ children, onClick }) {
     return (
         <button type="button" onClick={() => onClick(children)}>            
             {children}
         </button>
     );
 }
-
-export default Button;
