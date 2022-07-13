26e2a90247ab8beb9f9d140cdedda5ebeb98cf8e
moon-kun86 <108505117+moon-kun86@users.noreply.github.com>
2022-07-13 11:38:32 +0900

    Update Counter.jsx
    export default를 함수 앞에 선언하여 가독성을 높여주었습니다.

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -3,12 +3,10 @@ import React from 'react'; // 리액트 임포트 필수!
 
 // 카운터 컴포넌트
 // eslint-disable-next-line react/prop-types
-function Counter({ count, onClick }){
+export default function Counter({ count, onClick }){
     return (
         <button type="button" onClick={() => onClick()}> 
             Click me! ( { count } )
         </button>
     );
 }
-
-export default Counter;
