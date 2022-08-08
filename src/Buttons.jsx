1e544f1dc602278491c02437aa95a71fdfaabd8f
0-Chan <kyc4421194@gmail.com>
2022-08-08 18:10:05 +0900

    간단 카운터앱 (파일 분리하기)

---
+++ src/Buttons.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Button from './Button';
+
+export default function Buttons() {
+  return (
+    <p>
+      {[1, 2, 3, 4, 5].map((i) => (
+        <Button key={i}>
+          {i}
+        </Button>
+      ))}
+    </p>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+// 어떻게 나눌까?
+
+// Counter
+// Button
+// Buttons
+// Page
+// App
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
