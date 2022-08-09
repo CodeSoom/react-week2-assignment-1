9dd275b1a6b69fda7b0ebe807e63d1082061b70b
ChoJinmok <jjmok1127@gmail.com>
2022-08-09 20:56:42 +0900

    NumberButtons 에서 map 콜백함수의 매개변수명 변경

--- src/NumberButtons.jsx
+++ src/NumberButtons.jsx
@@ -5,9 +5,9 @@ import NumberButton from "./NumberButton";
 export default function NumberButtons({onClick}) {
     return (
       <p>
-        {[1, 2, 3, 4, 5].map((i) => (
-          <NumberButton key={i} onClick={onClick}>
-            {i}
+        {[1, 2, 3, 4, 5].map((number) => (
+          <NumberButton key={number} onClick={onClick}>
+            {number}
           </NumberButton>
         ))}
       </p>
