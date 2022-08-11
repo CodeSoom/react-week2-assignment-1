ab2301a10ffeefa243d7c5d81c9d35af44f63e75
0-Chan <kyc4421194@gmail.com>
2022-08-11 18:11:35 +0900

    Change variable addValue name again

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ export default function App() {
 
   const { count } = state;
 
-  function handleClick(addValue) {
+  function handleClick(addedValue) {
     setState({
-      count: count + addValue,
+      count: count + addedValue,
     });
   }
 
