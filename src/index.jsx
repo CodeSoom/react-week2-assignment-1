3ea7a0552cb90faa9f7ffd1e7f0ed8f5a3f49cab
SangYoonLee <sylagape1231@gmail.com>
2022-08-11 12:42:45 +0900

    2차 목표 달성! - 컴포넌트 분리
    - App 컴포넌트를 각 기능별로 분리하여 Counter, Button, Buttons, Page 4개의 컴포넌트를 생성했습니다.
    
    - 리뷰를 참고하여, ClickMe 클릭 이벤트와 NumberButton 클릭 이벤트를 하나의 함수로 관리할 수 있도록 다시 구현했습니다.

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,45 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+function Counter({ count, onClick }) {
+  return (
+    <button type="button" onClick={() => onClick(0)}>
+      Click me!
+      (
+      {count}
+      )
+    </button>
+  );
+}
+
+function Button({ keyNumber, onClick }) {
+  return (
+    <button type="button" onClick={() => onClick(keyNumber)}>
+      {keyNumber}
+    </button>
+  );
+}
+
+function Buttons({ onClick }) {
+  return (
+    <p>
+      {[1, 2, 3, 4, 5].map((i) => (
+        <Button keyNumber={i} onClick={onClick} />
+      ))}
+    </p>
+  );
+}
+
+function Page({ count, onClick }) {
+  return (
+    <div>
+      <p>Counter</p>
+      <Counter count={count} onClick={onClick} />
+      <Buttons onClick={onClick} />
+    </div>
+  );
+}
+
 function App() {
   const [number, setNumber] = useState({
     count: 0,
@@ -10,33 +49,19 @@ function App() {
 
   const { count } = number;
 
-  function handleClick() {
-    setNumber({ count: count + 1 });
-  }
-
-  function handleClickNumber(value) {
+  function handleClick(value) {
+    if (!value) {
+      setNumber({ count: count + 1 });
+      return;
+    }
     setNumber({ count: count + value });
   }
 
   return (
-    <div>
-      <p>Counter</p>
-      <p>
-        <button type="button" onClick={() => handleClick()}>
-          Click me!
-          (
-          {count}
-          )
-        </button>
-      </p>
-      <p>
-        {[1, 2, 3, 4, 5].map((i) => (
-          <button key={i} type="button" onClick={() => handleClickNumber(i)}>
-            {i}
-          </button>
-        ))}
-      </p>
-    </div>
+    <Page
+      count={count}
+      onClick={handleClick}
+    />
   );
 }
 
