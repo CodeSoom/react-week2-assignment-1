dd84a017832c6e81ed2b89626c94293bdcf4e128
sejineeee <sejinee0918@gmail.com>
2022-10-19 20:01:42 +0900

    review: App 컴포넌트 분리

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import CounterPage from './pages/CounterPage';
 
-function App() {
-  const [state, setState] = useState({
-    count: 0,
-  });
+import App from './App';
 
-  const { count } = state;
-
-  const numberList = [1, 2, 3, 4, 5];
-
-  const handleClick = (increasingValue) => {
-    setState({
-      count: count + increasingValue,
-    });
-  };
-
-  return (
-    <div>
-      <CounterPage count={count} handleClick={handleClick} numberList={numberList} />
-    </div>
-  );
-}
 ReactDOM.render(<App />, document.getElementById('app'));
