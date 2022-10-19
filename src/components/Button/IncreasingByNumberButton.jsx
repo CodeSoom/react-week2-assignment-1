7a3765117a39fce6fb2db455e99813218686e0e2
sejineeee <sejinee0918@gmail.com>
2022-10-19 19:13:14 +0900

    review: 파일명 수정
    IncreasingByNumberButton에서 NumberButton으로 파일명 수정

--- src/components/Button/IncreasingByNumberButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function IncreasingByNumberButton({ number, handleClick }) {
-  return (
-    <button type="button" onClick={() => handleClick(number)}>{number}</button>
-  );
-}
-
-export default IncreasingByNumberButton;
