ebe9b3c5131225eb55a187025cea94305b65ea18
sejineeee <sejinee0918@gmail.com>
2022-10-19 20:00:11 +0900

    review: App 컴포넌트 생성
    index 파일에 있던 App 컴포넌트를 분리하여 생성

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CounterPage from './pages/CounterPage';
+
+function App() {
+  return (
+    <div>
+      <CounterPage />
+    </div>
+  );
+}
+
+export default App;
