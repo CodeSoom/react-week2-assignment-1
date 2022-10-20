2605b564d49167e6b7f790e77435dde2c7d1c628
hyeji07 <zindex_ann@naver.com>
2022-10-20 18:54:28 +0900

    refactor:Props Drilling해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
-import { useState } from 'react';
-
 import CounterPage from './pages/CounterPage';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
-  function handleClickNumber(value) {
-    setCount(count + value);
-  }
-
   return (
-    <CounterPage
-      count={count}
-      onClick={handleClick}
-      handleClickNumber={handleClickNumber}
-    />
+    <CounterPage />
   );
 }
 
--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
+
 import Counter from '../components/Counter';
 import Buttons from '../components/buttons/Buttons';
 
-function CounterPage({ count, onClick, handleClickNumber }) {
+function CounterPage() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  function handleClickNumber(value) {
+    setCount(count + value);
+  }
   return (
     <div>
       <p>assignment 1</p>
       <p>Counter</p>
       <Counter
         count={count}
-        onClick={onClick}
+        onClick={handleClick}
       />
       <Buttons onClick={handleClickNumber} />
     </div>
