346a3185aa0a44068189e02eb23e00924793039b
naji0001 <najiwon001@gmail.com>
2022-12-14 19:57:31 +0900

    Fix component
    1. Delete unnecessary Page component
    2. Separate App component from index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,5 @@
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import Page from './components/Page';
 
-function App() {
-  const [state, setState] = useState({
-    count: 0,
-  });
+import App from './App';
 
-  const { count } = state;
-
-  function handleClickButton(number = 1) {
-    setState({
-      count: count + number,
-    });
-  }
-
-  return (
-    <Page
-      count={count}
-      onClick={handleClickButton}
-    />
-  );
-}
 ReactDOM.render(<App />, document.querySelector('#app'));
