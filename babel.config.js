2a55f965d59bba01a8fa90f62ff91ed636ef970c
naji0001 <najiwon001@gmail.com>
2022-12-14 19:33:51 +0900

    Add babel support for automatically import react
    Remove import react clause

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,10 @@ module.exports = {
         },
       },
     ],
-    '@babel/preset-react',
+    [
+      '@babel/preset-react', {
+        runtime: 'automatic',
+      },
+    ],
   ],
 };
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Button({ number, onClick }) {
   return (
     <button type="button" onClick={() => onClick(number)}>
