25da46256b68bfd9464a3c1168ef8bdf0dc80640
ctaaag <qudxor102@gmail.com>
2023-05-10 17:56:59 +0900

    fix: 린트 에러 해결

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 import ClickMe from './ClickMe.jsx';
 import Button from './Button.jsx';
 
 function App() {
   const [counterNumber, setCounterNumber] = useState(0);
-
   function handlerClickButton({ number }) {
     setCounterNumber(counterNumber + number);
   }
