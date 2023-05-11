016837834aa6697bd96a40fc8b9e36e05994b670
ctaaag <qudxor102@gmail.com>
2023-05-11 15:37:34 +0900

    fix: 린트에러 해결

--- src/components/ClickMeButton.jsx
+++ src/components/ClickMeButton.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default function ClickMeButton({ counterNumber, handlerClickButton }) {
   return (
     <button type="button" onClick={() => handlerClickButton({ number: 1 })}>
-      Click me ({counterNumber})
+      Click me (
+      {counterNumber}
+      )
     </button>
   );
 }
